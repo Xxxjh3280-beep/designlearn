b8c70ce9fa8dbcd26b4a4ee36ebf77f72f6c92c4
Xxxjh <Xxxjh3280@163.com>
2025-11-12 21:14:54 +0800

    basename: import.meta.env.BASE_URL in react router

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import AppLayout from './components/AppLayout'; 
+import AppLayout from './components/AppLayout';
 import ProductPage from './pages/ProductPage'
 import SearchPage from './pages/SearchPage';
 
@@ -16,25 +16,25 @@ const router = createBrowserRouter([
   {
     // path: "/",
     // 根路由使用 AppLayout 作为公共布局
-    Component: AppLayout, 
+    Component: AppLayout,
     // 定义子路由
     children: [
-        {
-            // index: true 表示这是父路径 (path: "/") 的默认子路由
-            index: true, 
-            Component: HomePage, // / 路径下渲染 HomePage
-        },
-        // 示例：添加一个 /about 页面作为子路由
-        {
-            path: "about", // 完整路径是 /about
-            element: <div>这是一个关于页面，位于 AppLayout 内部。</div>,
-        },
-        {
-          path: "product/:productId", // 完整路径是 /about
-          Component: ProductPage,
+      {
+        // index: true 表示这是父路径 (path: "/") 的默认子路由
+        index: true,
+        Component: HomePage, // / 路径下渲染 HomePage
       },
-        {
-        path: "search", 
+      // 示例：添加一个 /about 页面作为子路由
+      {
+        path: "about", // 完整路径是 /about
+        element: <div>这是一个关于页面，位于 AppLayout 内部。</div>,
+      },
+      {
+        path: "product/:productId", // 完整路径是 /about
+        Component: ProductPage,
+      },
+      {
+        path: "search",
         Component: SearchPage, // 或者 element: <div>...</div>
       },
       {
@@ -44,29 +44,31 @@ const router = createBrowserRouter([
           {
             index: true,
             // 当访问 /user 时显示的默认内容
-            element: <div>用户中心首页</div>, 
+            element: <div>用户中心首页</div>,
           },
           {
             path: "profile", // 完整路径是 /user/profile
-            element: <div>用户个人资料页</div>, 
+            element: <div>用户个人资料页</div>,
           },
-    ]
+        ]
+      },
+    ],
   },
-],
-  }
-]);
+],{
+  basename: import.meta.env.BASE_URL
+});
 
 // 2. 使用 ReactDOM.createRoot 渲染应用
-const rootElement = document.getElementById("root"); 
+const rootElement = document.getElementById("root");
 // 确保您的 index.html 中有一个 id="root" 的 div
 
 if (rootElement) {
-    ReactDOM.createRoot(rootElement).render(
-      <React.StrictMode>
-        {/* 3. 使用 RouterProvider 包装应用并传入 router 配置 */}
-        <RouterProvider router={router} />
-      </React.StrictMode>
-    );
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      {/* 3. 使用 RouterProvider 包装应用并传入 router 配置 */}
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
 } else {
-    console.error("未找到 id 为 'root' 的 DOM 元素。");
+  console.error("未找到 id 为 'root' 的 DOM 元素。");
 }
